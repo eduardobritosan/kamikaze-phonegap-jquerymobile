e884c0291a5a51d86f24f0b5db4313eb6caac691
Eduardo Brito Sanchez <eduardo_brito27@hotmail.com>
2015-12-04 10:11:11 +0000

    Accelerometer.js #btnStart

---
+++ www/js/accelerometer.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  var watchID = 0;
+  function onSuccess(acceleration) {
+    var element 
+  }
+  $("#btnStart").on("touchstart", function() {
+    if(watchId == 0){
+      watchId = navigator.accelerometer.watchAcceleration()
+    }
+  });
+});
