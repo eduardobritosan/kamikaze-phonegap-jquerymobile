8afaa3ccb57814d8b02a0ffcb7760d3214a57a85
Eduardo Brito Sanchez <eduardo_brito27@hotmail.com>
2015-12-04 12:21:55 +0000

    Acelerometro programado, aun no funciona

--- www/js/accelerometer.js
+++ www/js/accelerometer.js
@@ -1,11 +1,34 @@
-$(document).ready(function() {
-  var watchID = 0;
-  function onSuccess(acceleration) {
-    var element 
-  }
-  $("#btnStart").on("touchstart", function() {
+
+var options = { frequency: 300 };
+var watchId = 0;
+var maxValue = 0;
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+  $("#btnStart").on("click", function() {
     if(watchId == 0){
-      watchId = navigator.accelerometer.watchAcceleration()
+      watchId = navigator.accelerometer.watchAcceleration(onSuccess, onError, options)
+    }
+    else {
+      navigator.accelerometer.clearWatch( watchId );
     }
   });
-});
+  console.log(navigator.accelerometer);
+}
+
+
+function onSuccess(acceleration) {
+  if(maxValue < acceleration.z){
+    maxValue = acceleration.z;
+  }
+  $('#txtX').attr( 'value', acceleration.x);
+  $('#txtY').attr( 'value', acceleration.y);
+  $('#txtZ').attr( 'value', acceleration.z);
+  $('#btnStart').html('Stop watching');
+  watchId
+}
+
+function onError() {
+  alert('onError!');
+}
